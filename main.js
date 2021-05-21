c9e87e35ef9c67b47bd76fdb605f1005dae027d5
hortencia718 <hortenciaecisneros@gmail.com>
2021-05-21 12:56:00 -0400

    add script and button

---
+++ main.js
@@ -0,0 +1,16 @@
+// document.getElementById("counter-el").innerText =5
+
+let count = 0
+
+ count =count + 1
+// let myAge = 49
+// let humanDogRatio =7
+// let myDogAge = myAge * humanDogRatio
+console.log(count)
+
+// steps to getting the count 
+// create the count starting at 0
+// listen for clicks on the increment add event listener
+// increment the count variable when the button is clicked
+// change the count-el in the html to reflect the new count
+
